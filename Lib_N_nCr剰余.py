48376afc1bb23a3ccc3c9b3a4844dd702e548165
ibtosmlin <ibt.osm@gmail.com>
2020-04-19 16:43:58 +0900

    update

--- Lib_N_nCr剰余.py
+++ Lib_N_nCr剰余.py
@@ -14,18 +14,19 @@ def nCr(n, r, mod):
         return 0
     r = min(r, n-r)
     return g1[n] * g2[r] * g2[n-r] % mod
-    
-ans = nCr(4, 2, mod)
+
+ret = nCr(4, 2, mod)
 
 #####################################
-# nCr % 10**9+7  N～10^9　k～10^5
+# nCr % 10**9+7  n～10^9　r～10^5
+# nは大きいが　rは小さい場合
 #####################################
-mod = 10 ** 9 + 7  # mod素数
-n,r = 4,3          # 出力の制限
+mod = 10 ** 9 + 7   # mod素数
+n,r = 4,3           # 出力の制限
 def nCr(n, r, mod):
     ret = [1]*(r+1)
     for i in range(1, r+1):
         ret[i] = (ret[i-1] * (n-i+1) * pow(i,mod-2,mod)) % mod
     return ret
 nCrl = nCr(n, r, mod)
-print(nCrl[1:])  #[4,6,4]
+print(nCrl[1:])     #[4,6,4]配列として出力
